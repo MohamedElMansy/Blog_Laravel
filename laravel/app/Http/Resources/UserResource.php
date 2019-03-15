2588a59f92d34a4319ca47a1b99aaaf281309280
mohamed elmansy <eng.mohamedelmansy94@gmail.com>
2019-03-15 18:36:57 +0200

    add creator name when get single post

--- laravel/app/Http/Resources/UserResource.php
+++ laravel/app/Http/Resources/UserResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,12 +15,8 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=> $this->id,
-            'title' => $this->title,
-            'descrption'=> $this->description,
-            'user_id' => $this->user_id,
-            'user' => new UserResource($this->user)
-
+            'id' => $this->id,
+            'name'=> $this->name,
         ];
     }
 }
